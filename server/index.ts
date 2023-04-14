077ffe16273a521d44f8752758fe10abc0930430
tisou1 <57515396+tisou1@users.noreply.github.com>
2023-04-14 14:36:04 +0800

    chore(server): 添加ts支持

--- server/index.ts
+++ server/index.ts
@@ -1,2 +1,17 @@
-const cc = 'ss'
-console.log('...')
+import express from 'express'
+
+const app = express()
+const PORT = 4000
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.get('/api', (req, res) => {
+  res.json({
+    messgae: 'Hello world',
+  })
+})
+
+app.listen(PORT, () => {
+  console.log(`服务启动在 http://localhost:${PORT}`)
+})
